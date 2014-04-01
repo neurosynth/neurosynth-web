94c4ea89bc4031faccb8291fa5f8374205fb420e
eruditescholar <me@rahulmb.com>
2014-04-01 09:49:59 -0500

    Working datatables

--- nsweb/frontend/controllers/studies.py
+++ nsweb/frontend/controllers/studies.py
@@ -49,18 +49,6 @@ def update_result(result, **kwargs):
             f['frequency'] = round(f['frequency'], 3)
         pass
 
-def datatables_postprocessor(result, **kwargs):
-    """ A wrapper for DataTables requests. Just takes the JSON object to be 
-    returned and adds the fields DataTables is expecting. This should probably be 
-    made a universal postprocessor and applied to all API requests that have a 
-    'datatables' key in the request arguments list. """
-    if 'sEcho' in request.args:
-        result['iTotalRecords'] = Study.query.count()  # Get the number of total records from DB
-        result['iTotalDisplayRecords'] = result.pop('num_results')
-        result['aaData'] = result.pop('objects')
-        result['sEcho'] = int(request.args['sEcho'])  # for security
-        # ...and so on for anything else we need
-
 def datatables_preprocessor(search_params=None, **kwargs):
     """ For DataTables AJAX requests, we may need to change the search params. 
     """
@@ -74,7 +62,52 @@ def datatables_preprocessor(search_params=None, **kwargs):
         # Convert the DataTables query parameters into what flask-restless wants
         # if 'iDisplayStart' in request.args:
 
+def datatables_postprocessor(result, **kwargs):
+    from itertools import islice
+    """ A wrapper for DataTables requests. Just takes the JSON object to be 
+    returned and adds the fields DataTables is expecting. This should probably be 
+    made a universal postprocessor and applied to all API requests that have a 
+    'datatables' key in the request arguments list. """
+    if 'sEcho' in request.args:
+        result['sEcho'] = int(request.args['sEcho']) # for security
+        result['iTotalRecords'] = Study.query.count()  # Get the number of total records from DB
+        result['iTotalDisplayRecords'] = result.pop('num_results')
+        result.pop('total_pages')
+        result.pop('page')
+        data = result.pop('objects')
+        data = [ [d['title'], d['authors'], d['journal'], d['year'], d['pmid']] for d in data ]
+        result['aaData'] = data
         
+        # ...and so on for anything else we need
+
+#     def index
+#         # @studies = Study.all
+#         # @num_studies = Study.count
+#         cols = ['title', 'authors', 'journal', 'year', 'pmid']
+#         param_sort_col = params[:iSortCol_0].nil? ? (params[:sort] || 'pmid') : cols[params[:iSortCol_0].to_i]
+#         param_sort_ord = (params[:sSortDir_0] || params[:order] || 'ASC')
+#         param_search = (params[:sSearch] || params[:q] || '')
+#         param_start = (params[:iDisplayStart] || params[:start] || 0).to_i
+#         param_per = (params[:iDisplayLength] || params[:perPage] || 20).to_i
+#         param_per = [param_per, 100].min   # Display max 100 records
+#         order = "#{param_sort_col} #{param_sort_ord}"
+#         count = Study.count
+#         studies = Study.scoped
+#         if !param_search.empty?
+#             studies = studies.where("MATCH(title, authors, journal) AGAINST ('*#{param_search}*' IN BOOLEAN MODE)")
+#             itdr = studies.size
+#         else
+#             itdr = count
+#         end
+#         studies = studies.order(order).limit(param_per).offset(param_start).select(['id'] + cols)
+#         @data = {
+#             :studies => studies,
+#             :iTotalRecords => count,
+#             :iTotalDisplayRecords => itdr,
+#             :sEcho => params[:sEcho]
+#         }
+#         respond_with(@data)
+#     end
 
 includes=['pmid',
         'title',
