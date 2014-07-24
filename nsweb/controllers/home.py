b4f60944ca76780ed26c9447b38ced070f607188
tyarkoni <tyarkoni@gmail.com>
2014-07-23 19:04:20 -0500

    first pass at landing page

--- nsweb/controllers/home.py
+++ nsweb/controllers/home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from nsweb.core import add_blueprint
+from nsweb.models import Feature
 import random
 
 bp = Blueprint('home', __name__)
@@ -10,7 +11,9 @@ def index():
     img = random.choice(['emotion', 'pain', 'language', 'attention', 'memory', 'motor', 'reward', 'sensation'])
     # image = Image.where(:name => "feature_#{img}_reverse").joins(:feature).select(['images.id', 'images.name', 'features.name', 'features.n_studies']).first
     # news_items = [] #NewsItem.paginate(:page => params[:page], :per_page => 3).order('created_at DESC')
-    return render_template('home/index.html.slim')
+    feature = random.choice(['reward', 'language', 'emotion', 'pain', 'working memory'])
+    feature = Feature.query.filter_by(name=feature).first()
+    return render_template('home/index.html.slim', feature=feature)
 
 add_blueprint(bp)
 
