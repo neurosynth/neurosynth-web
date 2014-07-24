699c0e7ba96ce0e41e52c4d9e3ff0c8c8a4fae28
tyarkoni <tyarkoni@gmail.com>
2014-07-23 19:17:58 -0500

    explicit ordering of includes in asset pipeline

--- nsweb/initializers/assets.py
+++ nsweb/initializers/assets.py
@@ -6,25 +6,35 @@ from os.path import join
 #sweetness: http://webassets.readthedocs.org/en/latest/builtin_filters.html
 
 # application css bundle
-css_nsweb = Bundle('scss/*.scss', filters='pyscss', output='css/main.css')
+css_nsweb = Bundle('scss/viewer.css.scss',
+                'scss/features.css.scss',
+                'scss/studies.css.scss',
+                'scss/locations.css.scss',
+                'scss/decode.css.scss',
+                'scss/genes.css.scss',
+                'scss/jquery-ui-override.css.scss',
+                'scss/style.scss',
+                filters='pyscss', output='css/main.css')
 
 # consolidated css bundle
 css_bundle = Bundle('css/jquery*.css',
-                  'css/style.css',
                     css_nsweb,
                   'css/font-awesome.min.css',
-                  'css/bootstrap*.min.css', 
+                  'css/bootstrap*.min.css',
+                  'css/style.css',
                   filters='cssmin', output='css/main.min.css')
 
 # Compile CoffeeScript to JS.
 # NOTE: ORDER MATTERS HERE! Must load init, then viewer, then everything else
 coffee_bundle = Bundle('coffee/init.js.coffee',
                  'coffee/viewer.js.coffee',
+                 'coffee/plots.js.coffee',
                  'coffee/studies.js.coffee',
                  'coffee/features.js.coffee',
                  'coffee/locations.js.coffee',
                  'coffee/decode.js.coffee',
                  'coffee/genes.js.coffee',
+                 'coffee/home.js.coffee',
                  filters='coffeescript', output='js/coffee.js')
 
 # consolidated JavaScript bundle
@@ -45,7 +55,7 @@ js_bundle = Bundle('js/lib/jquery.js',
 
 
 def init_assets(app):
-    # app.config['ASSETS_DEBUG'] = True
+    app.config['ASSETS_DEBUG'] = settings.DEBUG
     webassets = Environment(app)
     webassets.config['PYSCSS_STATIC_ROOT'] = join(STATIC_FOLDER, 'scss')
     webassets.config['PYSCSS_STATIC_URL'] = join(STATIC_FOLDER, 'css/main.css')
