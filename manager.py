0651c3ff9ea82d17fc7a415bff814b8c66ba2464
tyarkoni <tyarkoni@gmail.com>
2015-02-03 23:24:40 -0600

    list routes in manager

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 from nsweb.core import app, db, create_app
 from flask.ext.script import Manager
+from flask import url_for
 from flask.ext.migrate import Migrate, MigrateCommand
 from nsweb.models import *
 
@@ -7,6 +8,20 @@ create_app(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+# Found @ http://stackoverflow.com/questions/13317536/get-a-list-of-all-routes-defined-in-the-app
+@manager.command
+def list_routes():
+    import urllib
+
+    output = []
+    for rule in app.url_map.iter_rules():
+        methods = ','.join(rule.methods)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, rule))
+        output.append(line)
+
+    for line in sorted(output):
+        print(line)
 
 if __name__ == '__main__':
     manager.run()
