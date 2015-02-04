7a2f8d7ee7d32e6699743c07307d35e6bbf488c7
tyarkoni <tyarkoni@gmail.com>
2015-02-04 11:58:31 -0600

    controller and API routes for topic analyses

--- nsweb/controllers/analyses.py
+++ nsweb/controllers/analyses.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify, abort
-from nsweb.models import Analysis
+from nsweb.models.analyses import Analysis, AnalysisSet, TopicAnalysis, TermAnalysis
 from nsweb.core import add_blueprint
 from flask.helpers import url_for
 import simplejson as json
@@ -7,27 +7,11 @@ import re
 
 bp = Blueprint('analyses',__name__,url_prefix='/analyses')
  
-
+### ROUTES COMMON TO ALL ANALYSES ###
 def find_analysis(val):
     ''' Retrieve analysis by either id (when int) or name (when string) '''
-    return Analysis.query.get(val) if re.match('\d+$', val) else Analysis.query.filter_by(name=val).first()
-    
-@bp.route('/')
-def index():
-    return render_template('analyses/index.html.slim')
- 
-@bp.route('/analysis_names/')
-def get_analysis_names():
-    names = [f.name for f in Analysis.query.all()]  # optimize this later--select only names
-    return jsonify(data=names)
-
-@bp.route('/<string:val>/')
-def show(val):
-    analysis = find_analysis(val)
-    if analysis is None:
-        return render_template('analyses/missing.html.slim', analysis=val)
-    n_studies = analysis.frequencies.count()
-    return render_template('analyses/show.html.slim', analysis=analysis.name, n_studies=n_studies)
+    return Analysis.query.get(val) if re.match('\d+$', val) else \
+        Analysis.query.filter_by(name=val).first()
 
 @bp.route('/<string:val>/images')
 def get_images(val):
@@ -46,7 +30,7 @@ def get_images(val):
 
 @bp.route('/<string:val>/images/reverseinference/')
 def get_reverse_inference_image(val):
-    # Horrible hacks here to serve just reverse inference images with or 
+    # Horrible hacks here to serve just reverse inference images with or
     # without FDR; totally mucks up API pattern, so clean up later.
     analysis = find_analysis(val)
     fdr = ('nofdr' not in request.args.keys())
@@ -57,6 +41,7 @@ def get_reverse_inference_image(val):
 @bp.route('/<string:val>/studies')
 def get_studies(val):
     analysis = find_analysis(val)
+    print val, ", FOUND ANALYSIS:", analysis, analysis.frequencies.count()
     if 'dt' in request.args:
         data = []
         for f in analysis.frequencies:
@@ -64,9 +49,57 @@ def get_studies(val):
             link = '<a href={0}>{1}</a>'.format(url_for('studies.show',val=s.pmid),s.title)
             data.append([link, s.authors, s.journal, round(f.frequency, 3)])
         data = jsonify(data=data)
+        print data
     else:
+        print "NO DT MANG!"
         data = jsonify(studies=[s.pmid for s in analysis.studies])
     return data
 
-add_blueprint(bp)
+### TERM-SPECIFIC ROUTES ###
+@bp.route('/term_names/')
+def get_term_names():
+    names = [f.name for f in TermAnalysis.query.all()]  # optimize this later--select only names
+    return jsonify(data=names)
 
+@bp.route('/terms/')
+def list_terms():
+    return render_template('analyses/terms/index.html.slim')
+    
+@bp.route('/terms/<string:term>/')
+def show_term(term):
+    analysis = find_analysis(term)
+    if analysis is None:
+        return render_template('analyses/missing.html.slim', analysis=term)
+    return render_template('analyses/terms/show.html.slim', 
+                           analysis=analysis)
+
+### TOPIC-SPECIFIC ROUTES ###
+@bp.route('/topics/')
+def list_topic_sets():
+    topic_sets = AnalysisSet.query.filter(AnalysisSet.name.like('topic%'))
+    return render_template('analyses/topics/index.html.slim',
+                           topic_sets=topic_sets)
+
+@bp.route('/topics/<string:topic_set>/')
+def show_topic_set(topic_set):
+    topic_set = AnalysisSet.query.filter_by(name=topic_set).first()
+    return render_template('analyses/topics/show_set.html.slim',
+                           topic_set=topic_set)
+
+@bp.route('/topics/<string:topic_set>/<string:number>')
+def show_topic(topic_set, number):
+    topic = TopicAnalysis.query.join(AnalysisSet).filter(
+        TopicAnalysis.number == number, AnalysisSet.name == topic_set).first()
+    if topic is None:
+        return render_template('analyses/missing.html.slim', analysis=None)
+    terms = [t[0] for t in TermAnalysis.query.with_entities(TermAnalysis.name).all()]
+    top =topic.terms.split(', ')
+    def map_url(x):
+        if x in terms:
+            return '<a href="%s">%s</a>' % (url_for('analyses.show_term', term=x), x)
+        return x
+    topic.terms = ', '.join(map(map_url, top))
+    return render_template('analyses/topics/show.html.slim',
+                           analysis_set=topic.analysis_set, analysis=topic)
+
+add_blueprint(bp)
