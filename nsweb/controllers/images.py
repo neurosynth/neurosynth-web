d2bb79995c431c8489f1073c0cade574904d8885
tyarkoni <tyarkoni@gmail.com>
2015-02-05 21:16:41 -0600

    decoding refactor

--- nsweb/controllers/images.py
+++ nsweb/controllers/images.py
@@ -1,7 +1,9 @@
 from flask import send_from_directory, Blueprint, abort, request, jsonify, redirect, url_for, send_file
 from nsweb.models import Image, Analysis, Location, Download
+from nsweb.initializers import settings
 from nsweb.initializers.settings import IMAGE_DIR
 from nsweb.core import add_blueprint, db
+from nsweb.models.decodings import Decoding, DecodingSet
 import os
 import datetime as dt
 import re
@@ -35,8 +37,28 @@ def download(val, fdr=True):
     filename = image.image_file if fdr else image.uncorrected_image_file
     return send_nifti(filename)
 
+
+@bp.route('/<int:image>/decode/')
+def get_decoding_data(image, reference='term'):
+
+    # Fix this ugly-ass query...
+    dec = db.session.query(Decoding)\
+        .filter(Decoding.decoding_set_id == DecodingSet.id,
+                DecodingSet.name == reference,
+                Decoding.image_id == int(image)).first()
+
+    if dec is None:
+        abort(404)
+    data = open(os.path.join(settings.DECODING_RESULTS_DIR,
+                             dec.uuid + '.txt')).read().splitlines()
+    data = [x.split('\t') for x in data]
+    data = [{reference: f, 'r': round(float(v), 3)} for (f, v) in data]
+    return jsonify(data=data)
+
+
 @bp.route('/anatomical')
 def anatomical_underlay():
-    return send_nifti(os.path.join(IMAGE_DIR, 'anatomical.nii.gz'), 'anatomical.nii.gz')
+    return send_nifti(os.path.join(IMAGE_DIR, 'anatomical.nii.gz'),
+                      'anatomical.nii.gz')
 
 add_blueprint(bp)
