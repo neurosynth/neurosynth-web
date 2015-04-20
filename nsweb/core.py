af1bb533e67c216efac1aec189e62a858d166e4f
tyarkoni <tyarkoni@gmail.com>
2015-04-20 00:27:54 -0500

    set up for testing

--- nsweb/core.py
+++ nsweb/core.py
@@ -46,7 +46,7 @@ def setup_logging(logging_path, level):
         logger.addHandler(file_handler)
 
 
-def create_app(debug=True):
+def create_app(debug=True, test=False):
     '''creates app instance, db instance, and apimanager instance'''
 
     # Extra config stuff
@@ -56,7 +56,9 @@ def create_app(debug=True):
     if settings.SQL_ADAPTER == 'sqlite':
         db_uri = settings.SQLALCHEMY_SQLITE_URI
     elif settings.SQL_ADAPTER == 'mysql':
-        if settings.DEBUG:
+        if test:
+            db_to_use = settings.MYSQL_TEST_DB
+        elif settings.DEBUG:
             db_to_use = settings.MYSQL_DEVELOPMENT_DB
         else:
             db_to_use = settings.MYSQL_PRODUCTION_DB
