abb1499dc3056e52337eb754170448c1e836aa1f
tyarkoni <tyarkoni@gmail.com>
2015-06-08 18:28:15 -0500

    disable pickle serialization in celery

--- nsweb/initializers/__init__.py
+++ nsweb/initializers/__init__.py
@@ -1,13 +1,19 @@
 from celery import Celery, Task
 from nsweb.initializers import settings
 
+
 def make_celery(app):
     """ Initialize and return a new Celery instance """
     celery = Celery(app.import_name, broker=settings.CELERY_BROKER_URL,
                     backend=settings.CELERY_RESULT_BACKEND)
+    celery.conf.update(CELERY_ACCEPT_CONTENT=['json'],
+                       CELERY_TASK_SERIALIZER='json',
+                       CELERY_RESULT_SERIALIZER = 'json')
     TaskBase = celery.Task
+
     class ContextTask(TaskBase):
         abstract = True
+
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
