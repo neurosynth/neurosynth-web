3211a77a0a59e364c13925fc118cdc77963c130d
Tal Yarkoni <tyarkoni@gmail.com>
2018-07-12 14:03:50 -0500

    add analysis filter flag

--- setup_database.py
+++ setup_database.py
@@ -1,6 +1,7 @@
 from nsweb.core import create_app, db, app
 from nsweb.initializers import settings
 from nsweb.initializers import database_builder
+import pandas as pd
 import os
 
 
@@ -8,7 +9,7 @@ def main():
     create_app(debug=settings.DEBUG)
     import nsweb.models     #registers models
 
-    print "Initializing DatabaseBuilder..."
+    print("Initializing DatabaseBuilder...")
     # Create a new builder from a pickled Dataset instance and populate the DB
     # pass reset_dataset=False after first run to avoid rebuilding dataset
     dataset = settings.PICKLE_DATABASE
@@ -17,26 +18,30 @@ def main():
         db, dataset=dataset,
         studies=os.path.join(settings.ASSET_DIR, 'database.txt'),
         features=os.path.join(settings.ASSET_DIR, 'features.txt'),
-        reset_db=True, reset_dataset=True)
+        reset_db=True, reset_dataset=False)
         # reset_db=False, reset_dataset=False)
 
 
-    print "Adding analyses..."
+    print("Adding analyses...")
     if settings.PROTOTYPE:
         analyses = ['emotion', 'language', 'memory', 'pain', 'visual',
                     'attention', 'sensory']
+    elif settings.ANALYSIS_FILTER_FILE is not None:
+       filt = pd.read_csv(settings.ANALYSIS_FILTER_FILE, sep='\t', index_col=0)
+       analyses = filt[filt['keep'] == 1].index.tolist()
+       print("Keeping %d analysis terms." % len(analyses))
     else:
         analyses = None
 
     builder.add_term_analyses(analyses=analyses, add_images=True, reset=True)
 
-    print "Adding studies..."
+    print("Adding studies...")
     if settings.PROTOTYPE:
         builder.add_studies(analyses=analyses, limit=500)
     else:
         builder.add_studies(analyses=analyses)
 
-    print "Adding feature-based meta-analysis images..."
+    print("Adding feature-based meta-analysis images...")
     builder.generate_analysis_images(
         analyses=analyses, add_to_db=False, overwrite=True)
 
@@ -46,7 +51,7 @@ def main():
     # print "Adding cognitive atlas information for available terms..."
     # builder.add_cognitive_atlas_nodes()
 
-    print "Memory-mapping key image sets..."
+    print("Memory-mapping key image sets...")
     builder.memory_map_images(include=['terms'], reset=True)
 
     # print "Adding genes..."
