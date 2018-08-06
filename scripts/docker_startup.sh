99dae696ff4aba2a6b42e0fc31de6250647736ad
Tal Yarkoni <tyarkoni@gmail.com>
2018-08-06 11:49:36 -0700

    check for pickled dataset instead of asset dir

--- scripts/docker_startup.sh
+++ scripts/docker_startup.sh
@@ -3,8 +3,8 @@
 # Copy settings template to settings if latter doesn't exist
 [ ! -e nsweb/initializers/settings.py ] && cp nsweb/initializers/settings_template.py nsweb/initializers/settings.py
 
-# Run setup script if assets don't exist
-[ ! -d /data/assets ] && python3 /code/setup_database.py
+# Run setup script if pickled dataset doesn't exist
+[ ! -d /data/assets/neurosynth_dataset.pkl ] && python3 /code/setup_database.py
 
 # Start up gunicorn
 /usr/local/bin/gunicorn -w 2 -b :8000 runserver:app --reload --log-level debug
