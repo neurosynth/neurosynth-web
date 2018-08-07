93a49eb5b93736900d2213ff065f7708bc133155
Tal Yarkoni <tyarkoni@gmail.com>
2018-08-07 07:57:53 -0700

    fix conditional

--- scripts/docker_startup.sh
+++ scripts/docker_startup.sh
@@ -4,7 +4,7 @@
 [ ! -e nsweb/initializers/settings.py ] && cp nsweb/initializers/settings_template.py nsweb/initializers/settings.py
 
 # Run setup script if pickled dataset doesn't exist
-[ ! -d /data/assets/neurosynth_dataset.pkl ] && python3 /code/setup_database.py
+[ ! -e /data/assets/neurosynth_dataset.pkl ] && python3 /code/setup_database.py
 
 # Start up gunicorn
 /usr/local/bin/gunicorn -w 2 -b :8000 runserver:app --reload --log-level debug
